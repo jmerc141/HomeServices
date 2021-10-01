b7992deff4ae23628f0625f2ab05271fb201511b
Brian Harper <brianharperr@gmail.com>
2021-10-01 19:29:13 -0400

    MySQL Connection/Test Query

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const os = require('os');
-
+const mysql = require('mysql');
 const app = express();
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    port: "3306",
+    database: "homeservicetest"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to MySQL Server!")
+    con.query("SELECT * FROM test", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
