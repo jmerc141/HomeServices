01ac239c74f7568a638dcddd58564cb45c50069a
brianharperr <brianharperr@gmail.com>
2021-10-05 19:16:36 -0400

    Added RDS MySQL Server Connection

--- src/server/index.js
+++ src/server/index.js
@@ -4,19 +4,20 @@ const mysql = require('mysql');
 const app = express();
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
+    host: "homeservices-db.crq86z0yqsqt.us-east-2.rds.amazonaws.com",
+    user: "worker",
+    password: "workerpassword123",
     port: "3306",
-    database: "homeservicetest"
+    database: "homeservicesdb"
   });
   
+  let x;
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected to MySQL Server!")
-    con.query("SELECT * FROM test", function (err, result, fields) {
+    con.query("SELECT * FROM user_profiles", function (err, result, fields) {
       if (err) throw err;
-      console.log(result);
+        console.log(result);
     });
   });
 
